9867627c76cdd425f783acab4dad935d9898d2b2
aditiyadwiramadani88 <aditiyadwiramadani88@gamil.com>
2021-01-28 15:02:17 -0500

    OK

---
+++ Validor.py
@@ -0,0 +1,17 @@
+import re
+from flask import request, jsonify
+import logging 
+
+
+class Validators:
+	def __init__(self, request):
+		if request.data[request]:
+	def max(self, vslue, messsage=None):
+		pass
+	def email(messsage=None):
+	      pass
+	def required(self, messsage=None):
+	  	pass
+	def min(self, value, rmesssage=None):
+		pass
+    def regex(self, regex, messsage=None):
